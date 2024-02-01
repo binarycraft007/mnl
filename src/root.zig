3a7220b9e1a456b9cec2f50a4e0f5a399c295abd
binarycraft007 <elliot.huang.signed@gmail.com>
2024-02-01 16:41:16 +0800

    add all netlink headers

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,15 @@
 const std = @import("std");
 const testing = std.testing;
-pub const c = @cImport(@cInclude("libmnl/libmnl.h"));
+pub const c = @cImport({
+    @cInclude("libmnl/libmnl.h");
+    @cInclude("linux/ethtool_netlink.h");
+    @cInclude("linux/genetlink.h");
+    @cInclude("linux/hsr_netlink.h");
+    @cInclude("linux/nbd-netlink.h");
+    @cInclude("linux/rtnetlink.h");
+    @cInclude("linux/selinux_netlink.h");
+    @cInclude("linux/tipc_netlink.h");
+});
 
 test {
     const nl_maybe = c.mnl_socket_open(c.NETLINK_ROUTE);
